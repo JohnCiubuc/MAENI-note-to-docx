420d977d0f48d99006f8a91643e55ccf26ff9ebb
John Ciubuc <john.ciubuc@ttuhsc.edu>
2022-08-05 21:58:24 -0500

    switching from html to docx

--- maeni-note-to-pdf.py
+++ maeni-note-to-pdf.py
@@ -7,11 +7,13 @@ Created on Fri Aug  5 21:06:31 2022
 """
 
 import json
-
+from docx import Document 
 note = []
 
 def noteSection(item):
-    return [x for x in note if x['item'] == item]
+    text = [x['content'] for x in note if x['item'] == item][0]
+    # text = '<p>' + text + '</p>'
+    return text
 
 with open("../MAENI-dumps/student_note_content_202208052105.json", 'r') as f:
     jump = json.load(f)
@@ -30,36 +32,41 @@ css = """<style type=\"text/css\" style=\"display: none !important;\">
         .p1 { 
              line-height: 0px;
         }
+        p {
+  margin-top: 100px;
+  margin-bottom: 100px;
+  margin-right: 150px;
+  margin-left: 80px;
+}
         </style>"""
 cc = 'chief cadfasdf'
 
 
+
 for id in note_ids:
     note =  [x for x in note_db if x['student_note_id'] ==id] 
-    html = css +  f"""<br><br><div id=\"headerBox\">
-                <p class=\"alignleft\">
-                ID: "+{cc}+"<br>
-                </p>
-                </div>
-                <br><br><br><br>
-                <h2 align=left>Chief Complaint</h2>
-                <h2 align=left>History of Presenting Illness</h2>
-                <h2 align=left>Review of Systems</h2>
-                <h2 align=left>History</h2>
-                <h3 align=left>Past Medical History</h3>
-                <h3 align=left>Past Surgical History</h3>
-                <h3 align=left>Medications</h3>
-                <h3 align=left>Allergies</h3>
-                <h3 align=left>Family History</h3>
-                <h3 align=left>Social History</h3>
-                <h2 align=left>Physical Exam</h2>
-                <h3 align=left>Vitals</h3>
-                <h3 align=left>Exam</h3>
-                <h2 align=left>Data</h2>
-                <h2 align=left>Assessment and Plan</h2>
-                <h3 align=left>Summary Statement</h3>
-                <p1 align=justify>
-                ID: "+'Chief Complaint'+"<br>
-                </p1>"""
-    with open ('test.html', 'w') as f:
-        f.write(html)
+    print(noteSection(20))
+    # Add a heading of level 0 (Also called Title)
+    document = Document()
+    document.add_heading('Title for the document', 0)
+      
+    document.add_heading('Chief Complaint', 1)
+    document.add_paragraph(noteSection(20))
+    document.add_heading('History of Presenting Illness', 1)
+    document.add_paragraph(noteSection(21))
+    document.add_heading('Review of Systems', 1)
+    document.add_paragraph(noteSection(22))
+    document.add_heading('History', 1)
+    document.add_heading('Past Medical History', 2)
+    document.add_heading('Past Surgical History', 2)
+    document.add_heading('Medications', 2)
+    document.add_heading('Allergies', 2)
+    document.add_heading('Family History', 2)
+    document.add_heading('Social History', 2)
+    document.add_heading('Physical Exam', 1)
+    document.add_heading('Vitals', 2)
+    document.add_heading('Exam', 2)
+    document.add_heading('Data', 1)
+    document.add_heading('Assessment and Plan', 1)
+    document.add_heading('Summary Statement', 2)
+    document.save('test.docx')
