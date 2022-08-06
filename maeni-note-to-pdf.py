82d5d2c131d0fa1b54450c9239d12f67efed65bb
John Ciubuc <john.ciubuc@ttuhsc.edu>
2022-08-05 22:27:56 -0500

    Cleaned rest of css, simplified function, starting problem list

--- maeni-note-to-pdf.py
+++ maeni-note-to-pdf.py
@@ -10,19 +10,16 @@ import json
 from docx import Document 
 note = []
 
-def noteSection(item):
-    text = [x['content'] for x in note if x['item'] == item]
+def noteSection(item, subItem = -1):
+    if subItem == -1:   
+        text = [x['content'] for x in note if x['item'] == item]
+    else:
+        text = [x['content'] for x in note if x['item'] == item and x['subitem'] == subItem]
     if len(text) == 0:
         return ''
     text = text[0].strip()
     return text
 
-def noteSubSection(item, subItem):
-    text = [x['content'] for x in note if x['item'] == item and x['subitem'] == subItem]
-    if len(text) == 0:
-        return ''
-    text = text[0].strip()
-    return text
 with open("../MAENI-dumps/student_note_content_202208052105.json", 'r') as f:
     jump = json.load(f)
     
@@ -30,34 +27,12 @@ note_db = jump['student_note_content']
 note_ids = [329]
 # note_ids = [329,359,389]
 
-css = """<style type=\"text/css\" style=\"display: none !important;\">
-        .alignleft {
-        float: left;
-        }
-        .alignright {
-        float: right;
-        }
-        
-        .p1 { 
-             line-height: 0px;
-        }
-        p {
-  margin-top: 100px;
-  margin-bottom: 100px;
-  margin-right: 150px;
-  margin-left: 80px;
-}
-        </style>"""
-cc = 'chief cadfasdf'
-
-
-
 for id in note_ids:
     note =  [x for x in note_db if x['student_note_id'] ==id] 
     print(noteSection(20))
     # Add a heading of level 0 (Also called Title)
     document = Document()
-    document.add_heading('Title for the document', 0)
+    document.add_heading(f'Student Note ID: {id}', 0)
       
     document.add_heading('Chief Complaint', 1)
     document.add_paragraph(noteSection(20))
@@ -81,9 +56,9 @@ for id in note_ids:
     document.add_heading('Physical Exam', 1)
     document.add_heading('Vitals', 2)
     
-    doc = f"""Heart Rate: {noteSubSection(29,0)}, Blood Pressure: {noteSubSection(29,1)}
-Respiratory Rate: {noteSubSection(29,2)},  O2 Sat: {noteSubSection(29,3)}
-Weight: {noteSubSection(29,4)}, Height: {noteSubSection(29,5)}"""
+    doc = f"""Heart Rate: {noteSection(29,0)}, Blood Pressure: {noteSection(29,1)}
+Respiratory Rate: {noteSection(29,2)},  O2 Sat: {noteSection(29,3)}
+Weight: {noteSection(29,4)}, Height: {noteSection(29,5)}"""
     
     document.add_paragraph(doc)
     document.add_heading('Exam', 2)
@@ -93,10 +68,18 @@ Weight: {noteSubSection(29,4)}, Height: {noteSubSection(29,5)}"""
     document.add_heading('Assessment and Plan', 1)
     document.add_heading('Summary Statement', 2)
     
-    doc = f"""This is a {noteSubSection(38,1)} year old {noteSubSection(38,3)}, who is presenting today for {noteSubSection(38,6)}
-The patient has a pertinent history of {noteSubSection(38,9)}
-Patient's exam is remarkable for {noteSubSection(38,12)}
-Patient's data is remarkable for {noteSubSection(38,15)}"""
+    doc = f"""This is a {noteSection(38,1)} year old {noteSection(38,3)}, who is presenting today for {noteSection(38,6)}
+The patient has a pertinent history of {noteSection(38,9)}
+Patient's exam is remarkable for {noteSection(38,12)}
+Patient's data is remarkable for {noteSection(38,15)}"""
 
     document.add_paragraph(doc)
+    
+    # Needs looping later
+    problem_id=1
+    document.add_heading(f'Problem {problem_id}', 3)
+    document.add_heading('Differential DX}', 3)
+    document.add_heading('Diagnostic Plan', 3)
+    document.add_heading('Treatmnet Plan', 3)
+    
     document.save('test.docx')
